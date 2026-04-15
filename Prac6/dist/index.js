bebf8838a36637eda2f510ac3659c67d0d4047f2
C-Sm1th <smithluna422@gmail.com>
2026-04-15 10:11:02 -0600

    POO: prect 6

---
+++ Prac6/dist/index.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class CuentaBancaria {
+    constructor(saldoInicial) {
+        this._saldo = saldoInicial;
+    }
+    // 2. El GETTER: Para que el usuario pueda ver su saldo
+    get saldo() {
+        return this._saldo;
+    }
+    // 3. El SETTER: Con reglas de seguridad
+    set saldo(nuevoMonto) {
+        if (nuevoMonto < 0) {
+            console.log("Error: No puedes tener un saldo negativo.");
+        }
+        else {
+            this._saldo = nuevoMonto;
+            console.log("Saldo actualizado correctamente.");
+        }
+    }
+}
+// --- USO DEL CÓDIGO ---
+const miCuenta = new CuentaBancaria(100);
+// Usamos el GET (se ve como una variable normal, pero es un método)
+console.log("Tu saldo es: " + miCuenta.saldo);
+// Usamos el SET
+miCuenta.saldo = 500; // Funciona bien
+miCuenta.saldo = -20; // Dispara la validación de error
+//# sourceMappingURL=index.js.map
